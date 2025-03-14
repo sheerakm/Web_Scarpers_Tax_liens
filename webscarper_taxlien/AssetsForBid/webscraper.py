2f91a564a037258b878f9f172f0c5e4106d76d14
Sheerak <sheerak.m@berkeley.edu>
2025-03-14 16:37:52 -0700

    fixed the coordinates

--- webscarper_taxlien/AssetsForBid/webscraper.py
+++ webscarper_taxlien/AssetsForBid/webscraper.py
@@ -47,6 +47,7 @@ print("here")
 
 
 months = driver.find_elements(By.CLASS_NAME, "month")
+links = []
 
 for month in months:
     # Check if the div has data-year="2025"
@@ -64,23 +65,13 @@ for month in months:
                 if link_element:
                     link = link_element[0].get_attribute("href")
                     name = link_element[0].text
-                    print(f" - Visiting: {name} ({link})")
 
                     if link:
-                        print(f"Opening link: {link}")
-                        link_element.click()  # Click the link to open it
 
-                        # Wait for the new page to load (by waiting for an element on that page)
-                        WebDriverWait(driver, 10).until(
-                            EC.presence_of_element_located((By.TAG_NAME, "h1"))  # Example: Wait for an <h1> element
-                        )
+                        # fix this
+                        links.append(link)
 
-                        print("Page loaded successfully!")
 
-                        button = WebDriverWait(driver, 5).until(
-                            EC.element_to_be_clickable((By.XPATH,
-                                                        "//button[contains(text(), 'Click Here to Download Property List Spreadsheet')]"))
-                        )
 
 
                     # Go back to the main page
@@ -90,7 +81,15 @@ for month in months:
 
             except Exception as e:
                 print(f"Skipping an auction due to error: {e}")
-                break
-            break
+print(links)
+print(len(links))
+print("before final for loop")
+for link in links:
+    driver.get( link)
+    print(link)
+    button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH,
+        "//button[contains(text(), 'Click Here to Download Property List Spreadsheet')]"))
+                            )
+
 
 driver.quit()
